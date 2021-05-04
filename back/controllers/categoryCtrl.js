36226ba69b9a92002f9350eee8aae9986081314d
seung-ik <69374857+seung-ik@users.noreply.github.com>
2021-05-04 15:23:51 +0900

    CRUD category by admin

--- back/controllers/categoryCtrl.js
+++ back/controllers/categoryCtrl.js
@@ -9,6 +9,38 @@ const categoryCtrl = {
       return res.status(500).json({ msg: err.message });
     }
   },
+  createCategory: async (req, res) => {
+    try {
+      const { name } = req.body;
+
+      const category = await Category.findOne({ name });
+      if (category) return res.status(400).json({ msg: "This Category already exist" });
+
+      const newCategory = new Category({ name });
+
+      await newCategory.save();
+      res.json({ msg: "Create a Category" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  deleteCategory: async (req, res) => {
+    try {
+      await Category.findByIdAndDelete(req.params.id);
+      res.json({ msg: "delete a category" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  updateCategory: async (req, res) => {
+    try {
+      const { name } = req.body;
+      await Category.findByIdAndUpdate({ _id: req.params.id }, { name });
+      res.json({ msg: "update a category" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
 };
 
 module.exports = categoryCtrl;
--- back/middleware/authAdmin.js
+++ back/middleware/authAdmin.js
@@ -1,8 +1,8 @@
 const Users = require("../models/userModel");
 
-const authAdmin = (req, res, next) => {
+const authAdmin = async (req, res, next) => {
   try {
-    const user = Users.findOne({ id: req.user.id });
+    const user = await Users.findOne({ _id: req.user.id });
 
     if (user.role === 0) return res.status(400).json({ msg: "Admin resource denied" });
 
